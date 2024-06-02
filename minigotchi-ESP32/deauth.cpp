cca884ad86742901957f1eaec8c4d15de6ed9d60
dj1ch <tg.1ch0712@gmail.com>
2024-06-01 17:52:08 -0700

    deauth bypass

--- minigotchi-ESP32/deauth.cpp
+++ minigotchi-ESP32/deauth.cpp
@@ -12,6 +12,11 @@
  *
 */
 
+// for some dumb reason espressif really doesn't like us sending deauth frames, so i'll need to make this fix
+extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
+  return 0;
+}
+
 // default values before we start
 bool Deauth::running = false;
 std::vector<String> Deauth::whitelist = {};
