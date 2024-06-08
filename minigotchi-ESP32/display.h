b6c58c62b9e14571b5c3118ed27da6066d8a5187
ATOMNFT <itsmestener@gmail.com>
2024-06-08 17:16:59 -0500

    Added new files
    Had to redo repo due to to many conflicts.

--- minigotchi-ESP32/display.h
+++ minigotchi-ESP32/display.h
@@ -1,34 +1,38 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include "config.h"
-#include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 #include <SPI.h>
-#include <TFT_eSPI.h> // Defines the TFT_eSPI library for CYD
 #include <Wire.h>
+#include <TFT_eSPI.h>  // Defines the TFT_eSPI library
+#include "config.h"
 
 // SSD1306 screen
 #define SSD1306_SCREEN_WIDTH 128
 #define SSD1306_SCREEN_HEIGHT 64
 
 // Init screen
-#define SSD1306_OLED_RESET -1
+#define SSD1306_OLED_RESET -1 
 #define WEMOS_OLED_SHIELD_OLED_RESET 0 // GPIO0
 
 /** developer note:
- *
+ * 
  * the TFT_eSPI library may not require this, but these will be here regardless
- *
- */
+ * 
+*/
 
 // Define display parameters for CYD
 #define CYD_SCREEN_WIDTH 240
 #define CYD_SCREEN_HEIGHT 320
 
-// lilygo t-display-t3
-#define T_DISPLAY_S3_WIDTH 320
-#define T_DISPLAY_S3_HEIGHT 170
+// Define display parameters for TTGO_T_DISPLAY
+#define TTGO_T_DISPLAY_WIDTH 135
+#define TTGO_T_DISPLAY_HEIGHT 240
+
+// Define display parameters for M5StickC Plus and M5Cardputer
+#define M5STICK_SCREEN_WIDTH 135
+#define M5STICK_SCREEN_HEIGHT 240
 
 class Display {
 public:
@@ -38,9 +42,8 @@ public:
   ~Display();
 
 private:
-  static Adafruit_SSD1306 *adafruit_display;
-  static TFT_eSPI *tft_display; // Declare a static pointer to a TFT_eSPI object
-                                // to manage the TFT display.
+  static Adafruit_SSD1306* adafruit_display;
+  static TFT_eSPI* tft_display;  // Declare a static pointer to a TFT_eSPI object to manage the TFT display.
 };
 
 #endif // DISPLAY_H
