f8ed260adb263e004f40029e61d6e279a23aea21
ATOMNFT <itsmestener@gmail.com>
2024-06-11 14:08:34 -0500

    Apply restyled patch

--- User_Setup_Files/User_Setup_TTGO_NoTouch.h
+++ User_Setup_Files/User_Setup_TTGO_NoTouch.h
@@ -4,24 +4,24 @@
 // See SetupX_Template.h for all options available
 
 #define ST7789_DRIVER
-#define TFT_SDA_READ   // Display has a bidirectional SDA pin
+#define TFT_SDA_READ // Display has a bidirectional SDA pin
 
-#define TFT_WIDTH  135
+#define TFT_WIDTH 135
 #define TFT_HEIGHT 240
 
-#define CGRAM_OFFSET      // Library will add offsets required
+#define CGRAM_OFFSET // Library will add offsets required
 
-//#define TFT_MISO -1
+// #define TFT_MISO -1
 
-#define TFT_MOSI            19
-#define TFT_SCLK            18
-#define TFT_CS              5
-#define TFT_DC              16
-#define TFT_RST             23
+#define TFT_MOSI 19
+#define TFT_SCLK 18
+#define TFT_CS 5
+#define TFT_DC 16
+#define TFT_RST 23
 
-#define TFT_BL          4  // Display backlight control pin
+#define TFT_BL 4 // Display backlight control pin
 
-#define TFT_BACKLIGHT_ON HIGH  // HIGH or LOW are options
+#define TFT_BACKLIGHT_ON HIGH // HIGH or LOW are options
 
 #define LOAD_GLCD
 #define LOAD_FONT2
@@ -33,8 +33,8 @@
 
 #define SMOOTH_FONT
 
-//#define SPI_FREQUENCY  27000000
-  #define SPI_FREQUENCY  40000000
+// #define SPI_FREQUENCY  27000000
+#define SPI_FREQUENCY 40000000
 
-
-#define SPI_READ_FREQUENCY  6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
+#define SPI_READ_FREQUENCY                                                     \
+  6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
