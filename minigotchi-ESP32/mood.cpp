af717be23449a1ce1f06f9f2c534af432fe0c262
dj1ch <tg.1ch0712@gmail.com>
2024-06-16 17:18:50 -0700

    comment

--- minigotchi-ESP32/mood.cpp
+++ minigotchi-ESP32/mood.cpp
@@ -1,3 +1,21 @@
+/*
+* Minigotchi: An even smaller Pwnagotchi
+* Copyright (C) 2024 dj1ch
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * mood.cpp: handles and sets mood on the minigotchi
  */
@@ -14,21 +32,23 @@ String Mood::looking2 = Config::looking2;
 String Mood::neutral = Config::neutral;
 String Mood::sleeping = Config::sleeping;
 
-String Mood::getMood() {
+String Mood::getCurrentMood() {
   return currentMood;
 }
 
-void Mood::setMood(String face) {
+String Mood::getMood(String face) {
   if (face == Mood::happy) {
     currentMood = "happy";
   } else if (face == Mood::sad)  {
     currentMood = "sad";
   } else if (face == Mood::broken) {
-    currentMood = "broken"
+    currentMood = "broken";
   } else if (face == Mood::intense) {
     currentMood = "intense";
-  } else if (face == Mood::looking1 || face == Mood::looking2) {
-    currentMood = "looking";
+  } else if (face == Mood::looking1) {
+    currentMood = "looking1";
+  } else if (face == Mood::looking2) {
+    currentMood = "looking2"
   } else if (face == Mood::neutral) {
     currentMood = "neutral";
   } else if (face == Mood::sleeping) {
@@ -36,4 +56,33 @@ void Mood::setMood(String face) {
   } else {
     currentMood = " ";
   }
+
+  return currentMood;
 }
+
+String Mood::getCurrentFace() {
+  return currentFace;
+}
+
+String Mood::getFace(String mood) {
+  if (mood == "happy") {
+    currentFace = Mood::happy;
+  } else if (mood == "sad") {
+    currentFace = Mood:sad;
+  } else if (mood == "broken") {
+    currentFace = Mood::broken;
+  } else if (mood == "looking1") {
+    currentFace = Mood::looking1;
+  } else if (mood == "looking2") {
+    currentFace = Mood::looking2;
+  } else if (mood == "neutral") {
+    currentFace = Mood::neutral;
+  } else if (mood == "sleeping") {
+    currentFace = Mood::sleeping;
+  } else {
+    currentFace = " ";
+  }
+
+  return currentFace;
+}
+
--- minigotchi-ESP32/mood.h
+++ minigotchi-ESP32/mood.h
@@ -1,3 +1,21 @@
+/*
+* Minigotchi: An even smaller Pwnagotchi
+* Copyright (C) 2024 dj1ch
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /**
  * mood.cpp: header files for mood.cpp
  */
@@ -21,10 +39,13 @@ public:
     static String neutral;
     static String sleeping;
 
-    static String getMood();
-    static void setMood(String face);
+    static String getCurrentFace();
+    static String getCurrentMood();
+    static String getMood(String face);
+    static String getFace(String mood);
 private:
     static String currentMood;
+    static String currentFace;
 };
 
 #endif // MOOD_CPP
