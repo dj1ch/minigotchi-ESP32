1ada9bf154c193c846bd897dbbfebf516b6f2c50
ATOMNFT <itsmestener@gmail.com>
2024-06-26 15:47:40 -0500

    Updated User_setup files

--- User_Setup_Files/User_Setup_TTGO_NoTouch.h
+++ User_Setup_Files/User_Setup_TTGO_NoTouch.h
@@ -1,27 +1,32 @@
 // Setup for the TTGO T Display
-#define USER_SETUP_ID 25
-
-// See SetupX_Template.h for all options available
 
 #define ST7789_DRIVER
-#define TFT_SDA_READ // Display has a bidirectional SDA pin
+//#define ST7789_2_DRIVER
 
-#define TFT_WIDTH 135
+#define TFT_SDA_READ   // Display has a bidirectional SDA pin
+
+// For ST7789_2 and ILI9341 ONLY, define the colour order IF the blue and red are swapped on your display
+// Try ONE option at a time to find the correct colour order for your display
+
+ // #define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
+//  #define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
+
+#define TFT_BACKLIGHT_ON HIGH  // HIGH or LOW are options
+
+#define TFT_WIDTH  135
 #define TFT_HEIGHT 240
 
-#define CGRAM_OFFSET // Library will add offsets required
+#define CGRAM_OFFSET      // Library will add offsets required
 
-// #define TFT_MISO -1
+//#define TFT_MISO -1
 
-#define TFT_MOSI 19
-#define TFT_SCLK 18
-#define TFT_CS 5
-#define TFT_DC 16
-#define TFT_RST 23
+#define TFT_MOSI      19
+#define TFT_SCLK      18
+#define TFT_CS        5
+#define TFT_DC        16
+#define TFT_RST       23
+#define TFT_BL        4  // Display backlight control pin
 
-#define TFT_BL 4 // Display backlight control pin
-
-#define TFT_BACKLIGHT_ON HIGH // HIGH or LOW are options
 
 #define LOAD_GLCD
 #define LOAD_FONT2
@@ -33,8 +38,8 @@
 
 #define SMOOTH_FONT
 
-// #define SPI_FREQUENCY  27000000
-#define SPI_FREQUENCY 40000000
+//#define SPI_FREQUENCY  27000000
+#define SPI_FREQUENCY  40000000
 
-#define SPI_READ_FREQUENCY                                                     \
-  6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
+
+#define SPI_READ_FREQUENCY  6000000 // 6 MHz is the maximum SPI read speed for the ST7789V
